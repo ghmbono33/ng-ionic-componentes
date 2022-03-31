d57965c601c6dd82f8507ea2390419bfea888613
ghmbono33 <mexplaintome@gmail.com>
2022-03-31 06:05:42 +0200

    menu en json

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,93 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
-interface Componente {
-  icon: string;
-  name: string;
-  redirecTo: string;
-}
+import { Componente } from 'src/app/interfaces/interfaces';
+import { Observable } from 'rxjs';
+import { DataService } from '../../services/data.service';
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-  componentes: Componente[] = [
-    {
-      icon: 'american-football-outline',
-      name: 'Action Sheet',
-      redirecTo: '/action-sheet',
-    },
-    {
-      icon: 'alert-circle-outline',
-      name: 'Alert',
-      redirecTo: '/alert',
-    },
-    {
-      icon: 'beaker-outline',
-      name: 'Avatar',
-      redirecTo: '/avatar',
-    },
-    {
-      icon: 'radio-button-on-outline',
-      name: 'Buttons',
-      redirecTo: '/button',
-    },
-    {
-      icon: 'card-outline',
-      name: 'Cards',
-      redirecTo: '/card',
-    },
-    {
-      icon: 'checkmark-circle-outline',
-      name: 'Checkbox',
-      redirecTo: '/check',
-    },
-    {
-      icon: 'calendar-outline',
-      name: 'DateTime',
-      redirecTo: '/date-time',
-    },
-    {
-      icon: 'car-outline',
-      name: 'fab',
-      redirecTo: '/fab',
-    },
-    {
-      icon: 'grid-outline',
-      name: 'grid',
-      redirecTo: '/grid',
-    },
-    {
-      icon: 'infinite-outline',
-      name: 'Ininite-Scroll',
-      redirecTo: '/infinite',
-    },
-    {
-      icon: 'hammer-outline',
-      name: 'Input Forms',
-      redirecTo: '/input',
-    },
-    {
-      icon: 'list-outline',
-      name: 'List - Sliding',
-      redirecTo: '/list',
-    },
-    {
-      icon: 'reorder-three-outline',
-      name: 'List - Reorder',
-      redirecTo: '/list-reorder',
-    },
-    {
-      icon: 'refresh-circle-outline',
-      name: 'Loading',
-      redirecTo: '/loading',
-    },
-  ];
-  constructor(private menuController: MenuController) {}
+  componentes: Observable<Componente[]>;
 
-  ngOnInit() {}
+  // Si utilizaramos la forma más complicada de mostrar el menú
+  // constructor(private menuController: MenuController) {}
+
+  // Si utilizaramos la forma más complicada de mostrar el menú
+  // ngOnInit() {}
   // mostrarMenu() {
   //   // El menú está definido en app.component.html
   //   this.menuController.open('menuPrincipal');
   // }
+
+  constructor(private dataService: DataService) {}
+  ngOnInit(): void {
+    this.componentes = this.dataService.getMenuOpts();
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Componente } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -8,5 +9,9 @@ export class DataService {
   constructor(private http: HttpClient) {}
   getUsuarios() {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+  getMenuOpts() {
+    // Cargamos las opciones del menú
+    return this.http.get<Componente[]>('/assets/data/menu-opts.json');
   }
 }
